eb69c63e487bdcf12163f0451b00b3fba23ff356
Cody <cody.cg@gmail.com>
2011-03-18 13:54:01 -0600

    initial flash ideas

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ for (var i = 0; i < argv.length; i++) {
  
 var server = function(req, res) {
 	req.getUser = require('user').getUser;
-	res.render = templating.render;
-	res.redirectTo = templating.redirectTo;
-	res.request = req;
+	templating.setUpTemplating(req, res);
 	
 	util.log('Received ' + req.method + ' request for ' + req.url); 
 	var parsedUrl = url.parse(req.url, true);
